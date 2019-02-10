f8556e427c7e3aefbd3bb440294808a7c83ca0a3
kicyiu <albertokicyiu@gmail.com>
2019-02-10 20:00:15 -0300

    Initial Commit

---
+++ Carousel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by Alberto Tsang on 2/10/19.
+//  Copyright © 2019 kicyiusoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
